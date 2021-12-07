42a7f5e5c652a3694a7064afca8ef19db963a75c
Szeplaki <szeplaki.eva1@gmail.com>
2021-12-07 22:42:38 +0100

    Get coordinates of initial numbers

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,8 @@
 import random
 
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = list(alphabet)
 sudoku_array = []
 
 
@@ -35,8 +38,8 @@ def generate_sudoku_array():
 
 def print_board(board):
     
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    alpha = list(alphabet)
+    
+    
     print("      "+"   ".join([str(i) for i in range(1,len(board)+1)]) + "\n")
     print("    +" + "---+"*9)
     for i, row in enumerate(board):
@@ -69,6 +72,11 @@ def create_hard_board(indexlst):
     print(create_60_missing_hard)
     return create_60_missing_hard
 
+def create_dont_touch_numbers(indexlst):
+    rest_indicies_of_hard = sorted(indexlst[60:])
+    print(rest_indicies_of_hard)
+    return rest_indicies_of_hard
+
 
 def hide_numbers(merge_sudoku_array, lst_with_zeros):
     for i in range(len(merge_sudoku_array)):
@@ -81,7 +89,19 @@ def hide_numbers(merge_sudoku_array, lst_with_zeros):
 def divide_list_by_nine(lista):
     divided_list = [lista[x:x+9] for x in range(0, len(lista), 9)]
     return divided_list
-            
+
+
+def search_coordinates(nullásitott):
+    list_of_tuples = []
+    for i in range(len(nullásitott)):
+        for j in range(len(nullásitott[0])):
+            if nullásitott[i][j] != ".":
+                c = (i,)
+                f = j
+                tup = c + (f,)
+                list_of_tuples.append(tup)
+    return list_of_tuples
+
 
 def main():
     generate_sudoku_array()
@@ -89,14 +109,54 @@ def main():
     merged_array = merge_sudoku_array_together(sudoku_array)
     index_list = create_list_of_indices()
     hard_board = create_hard_board(index_list)
+    create_dont_touch_numbers(index_list)
     divided = hide_numbers(merged_array, hard_board)
     nullásitott = divide_list_by_nine(divided)
+    print(nullásitott)
     print_board(nullásitott)
+    dont_touch_coord = search_coordinates(nullásitott)
+    print(dont_touch_coord)
 
 
 main()
 
+def validate_user_choice(board):
+    user_choice = ask_for_input()
+    sorok_szama = len(board)
+    oszlopok_szama = len(board[0])
 
+    valid_rows = [i for i in alpha[0:oszlopok_szama]]
+    valid_cols = [i for i in range(1,sorok_szama+1)]
+    
+    
+    while True:
+        if user_choice == "Q":
+            print("Bye_bye!")
+            quit()
+        elif user_choice[0] in valid_rows and user_choice[1] in str(valid_cols):
+            return user_choice
+        else:
+            print("Invalid input")
+            user_choice = ask_for_input()
+            continue
+
+def ask_for_input():
+    user_choice = input("Which tile do you want to reveal? ").upper()
+    return user_choice
+
+    
+def set_coordinates(user_choice):
+
+    a = alpha.index(user_choice[0])
+    b = int(user_choice[1]) - 1
+    
+    return a,b
+
+
+    
+def write_first_letter(board,lst,x,y):
+    board[x][y] = lst[x][y]
+    return board[x][y]
 
 
 
