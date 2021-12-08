07fe05a0137a38723e1f831588c6c0471059b00b
Szeplaki <szeplaki.eva1@gmail.com>
2021-12-08 16:05:35 +0100

    Add ASCII arts

--- sudoku.py
+++ sudoku.py
@@ -221,12 +221,24 @@ def game_core(missing_squares):
             array_to_check = create_array_to_check(összevont_nullás)
             one_array = merge_sudoku_array_together(sudoku_array)
             if array_to_check == one_array:
-                print("You already guessed all the numbers!")
+                print("""  ______          _            _   _      _ 
+ |  ____|        | |          | | (_)    | |
+ | |__ __ _ _ __ | |_ __ _ ___| |_ _  ___| |
+ |  __/ _` | '_ \| __/ _` / __| __| |/ __| |
+ | | | (_| | | | | || (_| \__ \ |_| | (__|_|
+ |_|  \__,_|_| |_|\__\__,_|___/\__|_|\___(_)
+                                            """)
+                print("You guessed all the numbers!\n")
                 quit()
 
 
 def main():
-    print('HAVE FUN MATE! :D')
+    print("""                   __      __            ____            ____           __                  
+   _______  ______/ /___  / /____  __   / __/_  ______  / __/___ ______/ /_____  _______  __
+  / ___/ / / / __  / __ \/ //_/ / / /  / /_/ / / / __ \/ /_/ __ `/ ___/ __/ __ \/ ___/ / / /
+ (__  ) /_/ / /_/ / /_/ / ,< / /_/ /  / __/ /_/ / / / / __/ /_/ / /__/ /_/ /_/ / /  / /_/ / 
+/____/\__,_/\__,_/\____/_/|_|\__,_/  /_/  \__,_/_/ /_/_/  \__,_/\___/\__/\____/_/   \__, /  
+                                                                                   /____/   """)
     mode = add_game_mode()
     valid_input_mode(mode)
  
